d367cff31ef7ee5d92644509ff17075313cf017e
Tatsuya Katsushima <t.katsushima@opt.ne.jp>
2021-11-14 16:25:05 +0900

    ファーストコミット: 181,296

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -20,19 +20,133 @@ use std::time::SystemTime;
 #[allow(dead_code)]
 const MOD: usize = 1e9 as usize + 7;
 
+const ORDER_TOTAL: usize = 1_000;
+const SELECT_ORDER_NUM: usize = 50;
+
+const SIDE: usize = 800;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct Coord {
+    x: isize,
+    y: isize,
+}
+
+#[allow(dead_code)]
+impl Coord {
+    fn new(p: (isize, isize)) -> Self {
+        Coord { x: p.0, y: p.1 }
+    }
+    fn from_usize_pair(p: (usize, usize)) -> Self {
+        Coord {
+            x: p.0 as isize,
+            y: p.1 as isize,
+        }
+    }
+
+    fn in_field(&self) -> bool {
+        (0 <= self.x && self.x <= SIDE as isize) && (0 <= self.y && self.y <= SIDE as isize)
+    }
+
+    // ペアへの変換
+    fn to_pair(&self) -> (isize, isize) {
+        (self.x, self.y)
+    }
+
+    // マンハッタン距離
+    fn distance(&self, that: &Self) -> isize {
+        (self.x - that.x).abs() + (self.y - that.y).abs()
+    }
+
+    fn mk_4dir(&self) -> Vec<Self> {
+        let delta = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+
+        delta
+            .iter()
+            .map(|&p| self.plus(&Coord::new(p)))
+            .filter(|&pos| pos.in_field())
+            .collect()
+    }
+
+    fn com_to_delta(com: char) -> Self {
+        match com {
+            'U' => Coord::new((0, -1)),
+            'D' => Coord::new((0, 1)),
+            'L' => Coord::new((-1, 0)),
+            'R' => Coord::new((1, 0)),
+            _ => unreachable!(),
+        }
+    }
+
+    // 四則演算
+    fn plus(&self, that: &Self) -> Self {
+        Coord::new((self.x + that.x, self.y + that.y))
+    }
+    fn minus(&self, that: &Self) -> Self {
+        Coord::new((self.x - that.x, self.y - that.y))
+    }
+
+    fn access_matrix<'a, T>(&'a self, mat: &'a Vec<Vec<T>>) -> &'a T {
+        &mat[self.y as usize][self.x as usize]
+    }
+
+    fn set_matrix<T>(&self, mat: &mut Vec<Vec<T>>, e: T) {
+        mat[self.y as usize][self.x as usize] = e;
+    }
+}
+
+struct Request {
+    id: usize,
+    s: Coord,
+    g: Coord,
+}
+impl Request {}
+
 #[fastout]
 fn main() {
     let system_time = SystemTime::now();
-    let mut rng = thread_rng();
+    let mut _rng = thread_rng();
 
-    input! {
-        // n: usize,
-        // v: [isize; n],
+    let office: Coord = Coord::new((400, 400));
+
+    let mut reqs = Vec::with_capacity(ORDER_TOTAL);
+    for i in 1..=ORDER_TOTAL {
+        input! {
+            a: usize,
+            b: usize,
+            c: usize,
+            d: usize,
+        }
+
+        let s = Coord::from_usize_pair((a, b));
+        let g = Coord::from_usize_pair((c, d));
+
+        let req = Request { id: i, s, g };
+        reqs.push(req)
     }
 
-    let ans = 42;
+    let mut choice = vec![];
+    let mut route = vec![];
 
-    println!("{}", ans);
+    route.push(office.clone());
+    for i in 0..SELECT_ORDER_NUM {
+        let req = &reqs[i];
+        choice.push(req.id);
+        route.push(req.s.clone());
+        route.push(req.g.clone());
+    }
+    route.push(office.clone());
+
+    print!("{}", choice.len());
+    for req in &choice {
+        print!(" {}", req);
+    }
+    println!();
+
+    print!("{}", route.len());
+    for pos in &route {
+        print!(" {} {}", pos.x, pos.y);
+    }
+    println!();
 
     eprintln!("{}ms", system_time.elapsed().unwrap().as_millis());
 }
